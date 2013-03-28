073734621523368d3d1ea22fc7d7d22bb18cd850
elvin.lian <elvin.lian@foxmail.com>
2013-03-28 17:46:46 +0800

    更新了 cssmin, jsmin 版本，修改了minify.php程序只读本地文件

--- minify.php
+++ minify.php
@@ -1,120 +1,123 @@
 <?php
-/* 压缩 */
-$MINIFY = true;
-/* 默认cdn地址 */
-$YOUR_CDN = 'http://a.tbcdn.cn/';
+// 仅合并压缩 js, css
+
+// Assets 服务器根目录 -- 修改为你合适的目录
+if (!isset($ASSETS_ROOT_PATH)) $ASSETS_ROOT_PATH = dirname(dirname(__FILE__)) . '/';
+
+// 合并压缩后的文件存储位置，如果为空，不保存
+if (!isset($COMPILE_SAVE_PATH)) $COMPILE_SAVE_PATH = '';
+
+/* 是否压缩 */
+$MINIFY = TRUE;
 
 require 'jsmin.php';
 require 'cssmin.php';
-/**
- * set e-tag cache
- */
-function cache($etag){
-    $etag = $etag; //标记字符串，可以任意修改
-    if ($_SERVER['HTTP_IF_NONE_MATCH'] == $etag){
-        header('Etag:'.$etag,true,304);
-        exit;
-    }
-    else header('Etag:'.$etag);
-}
-
-function get_contents($url){
-    $ch =curl_init($url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $str =curl_exec($ch);
-    curl_close($ch);
-    if ($str !==false) {
-        return $str;
-    }else {
-        return '';
-    }  
-}
 
 //得到扩展名
-function get_extend($file_name) { 
-	$extend =explode("." , $file_name); 
-	$va=count($extend)-1;
-	return $extend[$va]; 
-} 
+function get_extend($file_name)
+{
+    $extend = explode(".", $file_name);
+    $va = count($extend) - 1;
+    return $extend[$va];
+}
 
-/**
- * logic begin
- */
+function is_cached($etag)
+{
+    return ((isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == $etag)) ? true : false;
+}
+
+$type = '';
+$last_modified_time = 0;
 $files = array();
-//cdn上存在的各种可能的文件类型
+$local_files = array(); // 本地存在的文件
 $header = array(
     'js' => 'Content-Type: application/x-javascript',
-    'css' => 'Content-Type: text/css',
-    'jpg' => 'Content-Type: image/jpg',
-    'gif' => 'Content-Type: image/gif',
-    'png' => 'Content-Type: image/png',
-    'jpeg' => 'Content-Type: image/jpeg',
-    'swf' => 'Content-Type: application/x-shockwave-flash'
+    'css' => 'Content-Type: text/css'
 );
-$type = '';
-foreach ($_REQUEST as $k => $v) {
-	//最常见的替换规则
-    $k = preg_replace(
-        array('/_(js|css|gif|png|jpg|jpeg|swf)$/','/yui\/2_8_0r4/','/yui\/3_0_0/','/(\d+)_(\d+)_(\d+)/','/(\d+)_(\d+)/','/_v(\d)/'),
-        array('.$1','yui/2.8.0r4','yui/3.0.0','$1.$2.$3','$1.$2','.v$1'),
-        trim($k,'/')
-    );
-	//在这里添加转换过头的各种情况
-	$k = str_replace('global.v5.css','global_v5.css',$k);
-	$k = str_replace('detail.v2.css','detail_v2.css',$k);
-	$k = str_replace('cubee_combo','cubee.combo',$k);
 
-    if(empty($type)) {
-		$type = get_extend($k);
+// 处理请求中附带的文件列表，得到原始数据
+$split_a = explode("??", $_SERVER['REQUEST_URI']);
+if (preg_match('/,/', $split_a[1])) { //多文件
+    $_tmp = explode(',', $split_a[1]);
+    foreach ($_tmp as $v) {
+        $files[] = $v;
     }
-	//文件存在
-    if(file_exists($k)) {
-		$in_str = file_get_contents($k);
-		//处理文本
-		if(preg_match('/js|css/',$type)){
-			//$files[] = file_get_contents($k);
-			if($MINIFY == true && $type == 'js'){
-				$files[] = JSMin::minify($in_str);
-			}else if($MINIFY == true && $type == 'css'){
-				$files[] = cssmin::minify($in_str);
-			}else{
-				$files[] = $in_str;
-			}
-		}else{
-			//处理非文本
-			$files[] = array($in_str);
-		}
-    }else{
-		//文件不存在
-		$in_sta = file($YOUR_CDN.$k);
-		//文本的处理
-		if(preg_match('/js|css/',$type)){
-			$files[] = '/* http://a.tbcdn.cn/'.$k.' */';
-			$inner_str = join('',$in_sta);
-			if($MINIFY == true && $type == 'js'){
-				$files[] = JSMin::minify($inner_str);
-			}else if($MINIFY == true && $type == 'css'){
-				$files[] = cssmin::minify($inner_str);
-			}else{
-				$files[] = $inner_str;
-			}
-		}else{
-			//非文本的处理
-			$files[] = $in_sta;
-		}
+} else { //单文件
+    $files[] = $split_a[1];
+}
+
+// 得到需要读取的文件列表
+foreach ($files as $k) {
+    //将开头的/和?去掉
+    $k = preg_replace(array('/^\//', '/\?.+$/'), array('', ''), $k);
+
+    //最后可能是一个逗号
+    if (!preg_match('/(\.js|\.css)$/', $k)) continue;
+
+    while (preg_match('/[^\/]+\/\.\.\//', $k)) {
+        $k = preg_replace(array('/[^\/]+\/\.\.\//'), array(''), $k, 1);
+    }
+
+    if (empty($type)) $type = get_extend($k);
+    if (!preg_match('/js|css/', $type)) continue;
+
+    if (file_exists($ASSETS_ROOT_PATH . $k)) {
+        $file_mtime = filemtime($ASSETS_ROOT_PATH . $k);
+        if ($file_mtime && ($file_mtime > $last_modified_time)) {
+            $last_modified_time = $file_mtime;
+        }
+        $local_files[] = $ASSETS_ROOT_PATH . $k;
     }
 }
 
-header("Expires: " . date("D, j M Y H:i:s", strtotime("now + 10 years")) ." GMT");
-//文本的处理
-header($header[$type]);//文件类型
-if(preg_match('/js|css/',$type)){
-	$result = join("",$files);
-}else{
-	//非文本的处理
-	$result = join("",$files[0]);
+if (!isset($header[$type])) {
+    header('status: 404');
+    echo '404 not found';
+    exit;
 }
-cache(md5($result));//etag,处理Etag是否多余?
+
+// 生成 etag
+$etag = md5($last_modified_time . implode('__', $local_files) . $last_modified_time);
+
+// output
+header($header[$type]);
+header("Expires: " . date("D, j M Y H:i:s", strtotime("now + 10 years")) . " GMT");
+header("Cache-Control: max-age=315360000");
+if ($last_modified_time > 0) {
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $last_modified_time) . ' GMT');
+}
+if (is_cached($etag)) { // 如果浏览器有缓存, 返回304
+    header('Etag:' . $etag, true, 304);
+    exit;
+} else {
+    header('Etag:' . $etag);
+}
+
+$compiled_file = '';
+if (!empty($COMPILE_SAVE_PATH)) {
+    $compiled_file = $COMPILE_SAVE_PATH . $etag . '.' . $type;
+}
+
+// 如果压缩后的缓存文件存在，直接读取
+if (!empty($compiled_file) && file_exists($compiled_file)) {
+    $result = file_get_contents($compiled_file);
+
+} else {
+    // 压缩 js, css
+    $css_compressor = new CSSmin();
+    $res_files = array();
+    foreach ($local_files as $k) {
+        $in_str = file_get_contents($k);
+        if ($MINIFY == true && $type == 'js') {
+            $res_files[] = JSMin::minify($in_str);
+        } else if ($MINIFY == true && $type == 'css') {
+            $res_files[] = $css_compressor->run($in_str, 2000);
+        } else {
+            $res_files[] = $in_str;
+        }
+    }
+    $result = join("", $res_files);
+    file_put_contents($compiled_file, $result);
+}
 echo $result;
-?>
+exit;
